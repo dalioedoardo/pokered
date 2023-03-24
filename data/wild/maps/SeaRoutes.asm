8858ab917b610d6fc0fd57e6e1e5b11f8fc42d18
dalioedoardo <dalioedoardo@gmail.com>
2023-03-24 17:35:49 +0100

    aggiornati i selvatici di SeaRoutes

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -3,14 +3,14 @@ SeaRoutesWildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
-	db 30, TENTACOOL
-	db 35, LAPRAS ;Harzen 18/03/2023
-	db 40, TENTACOOL
+	db 25, TENTACOOL ;Harzen 24/03/2023
+	db 28, TENTACOOL ;Harzen 24/03/2023
+	db 20, TENTACOOL ;Harzen 24/03/2023
+	db 10, HORSEA ;Harzen 24/03/2023
+	db 12, POLIWAG ;Harzen 24/03/2023
+	db 18, PSYDUCK ;Harzen 24/03/2023
+	db 15, GOLDEEN ;Harzen 24/03/2023
+	db 35, TENTACRUEL ;Harzen 24/03/2023
+	db 40, TENTACRUEL ;Harzen 24/03/2023
+	db 40, POLIWRATH ;Harzen 24/03/2023
 	end_water_wildmons
