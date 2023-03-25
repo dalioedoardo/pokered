8cdcc3018a42596bee2b891a9d20e1d2f25dc5f9
dalioedoardo <dalioedoardo@gmail.com>
2023-03-25 07:00:29 +0100

    Reso vulpix/growlithe esclusivo più raro nella route 8

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -9,8 +9,8 @@ IF DEF(_RED)
 	db 20, MANKEY
 	db 19, EKANS
 	db 17, GROWLITHE
-	db 15, VULPIX ;Harzen 18/03/2023
 	db 18, GROWLITHE
+	db 15, VULPIX ;Harzen 18/03/2023
 ENDC
 IF DEF(_BLUE)
 	db 18, KOFFING ;Harzen 23/03/2023
@@ -20,8 +20,8 @@ IF DEF(_BLUE)
 	db 20, MEOWTH
 	db 19, SANDSHREW
 	db 17, VULPIX
-	db 15, GROWLITHE ;Harzen 18/03/2023
 	db 18, VULPIX
+	db 15, GROWLITHE ;Harzen 18/03/2023
 ENDC
 	end_grass_wildmons
 
