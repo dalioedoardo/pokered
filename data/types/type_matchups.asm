bc3322d2c80bd3328a06f679fdcee0e26f5a75db
dalioedoardo <dalioedoardo@gmail.com>
2023-04-04 19:36:03 +0200

    cambiamento superefficacia tipi

--- data/types/type_matchups.asm
+++ data/types/type_matchups.asm
@@ -1,39 +1,45 @@
 TypeEffects:
 	;  attacker,     defender,     *=
-	db WATER,        FIRE,         SUPER_EFFECTIVE
+	db WATER,        WATER,        NOT_VERY_EFFECTIVE
+  db WATER,        FIRE,         SUPER_EFFECTIVE
+	db WATER,        ROCK,         SUPER_EFFECTIVE
+	db WATER,        ICE,          NOT_VERY_EFFECTIVE ;harzen 03/04/2023
+	db WATER,        GRASS,        NOT_VERY_EFFECTIVE
+	db WATER,        GROUND,       NOT_VERY_EFFECTIVE  ;Harzen 03/04/2023
+	db WATER,        DRAGON,       NOT_VERY_EFFECTIVE
+	db FIRE,         FIRE,         NOT_VERY_EFFECTIVE
 	db FIRE,         GRASS,        SUPER_EFFECTIVE
 	db FIRE,         ICE,          SUPER_EFFECTIVE
-	db GRASS,        WATER,        SUPER_EFFECTIVE
+	db FIRE,         WATER,        NOT_VERY_EFFECTIVE
+	db FIRE,         BUG,          SUPER_EFFECTIVE
+	db FIRE,         ROCK,         NOT_VERY_EFFECTIVE
+  db FIRE,         GROUND,       NOT_VERY_EFFECTIVE  ;Harzen 03/04/2023
+	db FIRE,         DRAGON,       NOT_VERY_EFFECTIVE
+	db GRASS,        GRASS,        NOT_VERY_EFFECTIVE
+  db GRASS,        WATER,        SUPER_EFFECTIVE
+	db GRASS,        FIRE,         NOT_VERY_EFFECTIVE
+	db GRASS,        GROUND,       SUPER_EFFECTIVE
+	db GRASS,        BUG,          NOT_VERY_EFFECTIVE
+	db GRASS,        POISON,       NOT_VERY_EFFECTIVE
+	;db GRASS,        ROCK,         SUPER_EFFECTIVE  ;Harzen 03/04/2023 commentata
+	;db GRASS,        FLYING,       NOT_VERY_EFFECTIVE ;Harzen 03/2023 commentata
 	db ELECTRIC,     WATER,        SUPER_EFFECTIVE
-	db WATER,        ROCK,         SUPER_EFFECTIVE
 	db GROUND,       FLYING,       NO_EFFECT
-	db WATER,        WATER,        NOT_VERY_EFFECTIVE
-	db FIRE,         FIRE,         NOT_VERY_EFFECTIVE
 	db ELECTRIC,     ELECTRIC,     NOT_VERY_EFFECTIVE
 	db ICE,          ICE,          NOT_VERY_EFFECTIVE
-	db GRASS,        GRASS,        NOT_VERY_EFFECTIVE
+  db ICE,          ROCK,         NOT_VERY_EFFECTIVE ;Harzen 03/04/2023
 	db PSYCHIC_TYPE, PSYCHIC_TYPE, NOT_VERY_EFFECTIVE
-	db FIRE,         WATER,        NOT_VERY_EFFECTIVE
-	db GRASS,        FIRE,         NOT_VERY_EFFECTIVE
-	db WATER,        GRASS,        NOT_VERY_EFFECTIVE
 	db ELECTRIC,     GRASS,        NOT_VERY_EFFECTIVE
 	db NORMAL,       ROCK,         NOT_VERY_EFFECTIVE
 	db NORMAL,       GHOST,        NO_EFFECT
 	db GHOST,        GHOST,        SUPER_EFFECTIVE
-	db FIRE,         BUG,          SUPER_EFFECTIVE
-	db FIRE,         ROCK,         NOT_VERY_EFFECTIVE
-	db WATER,        GROUND,       SUPER_EFFECTIVE
 	db ELECTRIC,     GROUND,       NO_EFFECT
 	db ELECTRIC,     FLYING,       SUPER_EFFECTIVE
-	db GRASS,        GROUND,       SUPER_EFFECTIVE
-	db GRASS,        BUG,          NOT_VERY_EFFECTIVE
-	db GRASS,        POISON,       NOT_VERY_EFFECTIVE
-	db GRASS,        ROCK,         SUPER_EFFECTIVE
-	db GRASS,        FLYING,       NOT_VERY_EFFECTIVE
 	db ICE,          WATER,        NOT_VERY_EFFECTIVE
 	db ICE,          GRASS,        SUPER_EFFECTIVE
 	db ICE,          GROUND,       SUPER_EFFECTIVE
 	db ICE,          FLYING,       SUPER_EFFECTIVE
+	db ICE,          FIRE,         NOT_VERY_EFFECTIVE ;Harzen 03/04/2023
 	db FIGHTING,     NORMAL,       SUPER_EFFECTIVE
 	db FIGHTING,     POISON,       NOT_VERY_EFFECTIVE
 	db FIGHTING,     FLYING,       NOT_VERY_EFFECTIVE
@@ -42,10 +48,12 @@ TypeEffects:
 	db FIGHTING,     ROCK,         SUPER_EFFECTIVE
 	db FIGHTING,     ICE,          SUPER_EFFECTIVE
 	db FIGHTING,     GHOST,        NO_EFFECT
+	db FIGHTING,     WATER,        NOT_VERY_EFFECTIVE ;Harzen 03/04/2023
 	db POISON,       GRASS,        SUPER_EFFECTIVE
 	db POISON,       POISON,       NOT_VERY_EFFECTIVE
-	db POISON,       GROUND,       NOT_VERY_EFFECTIVE
-	db POISON,       BUG,          SUPER_EFFECTIVE
+	db POISON,       GROUND,       SUPER_EFFECTIVE ;Harzen 03/04/2023 cambiata da not_very_effective a super_effective
+	;db POISON,      BUG,          SUPER_EFFECTIVE ;Harzen 03/04/2023 commentata
+	db POISON,       WATER,        SUPER_EFFECTIVE ;Harzen 03/04/2023
 	db POISON,       ROCK,         NOT_VERY_EFFECTIVE
 	db POISON,       GHOST,        NOT_VERY_EFFECTIVE
 	db GROUND,       FIRE,         SUPER_EFFECTIVE
@@ -63,11 +71,12 @@ TypeEffects:
 	db PSYCHIC_TYPE, POISON,       SUPER_EFFECTIVE
 	db BUG,          FIRE,         NOT_VERY_EFFECTIVE
 	db BUG,          GRASS,        SUPER_EFFECTIVE
-	db BUG,          FIGHTING,     NOT_VERY_EFFECTIVE
-	db BUG,          FLYING,       NOT_VERY_EFFECTIVE
+	;db BUG,          FIGHTING,     NOT_VERY_EFFECTIVE ;Harzen 03/04/2023 commentata
+	;db BUG,          FLYING,       NOT_VERY_EFFECTIVE
 	db BUG,          PSYCHIC_TYPE, SUPER_EFFECTIVE
 	db BUG,          GHOST,        NOT_VERY_EFFECTIVE
 	db BUG,          POISON,       SUPER_EFFECTIVE
+	db BUG,          ROCK,         NOT_VERY_EFFECTIVE ;Harzen 03/04/2023
 	db ROCK,         FIRE,         SUPER_EFFECTIVE
 	db ROCK,         FIGHTING,     NOT_VERY_EFFECTIVE
 	db ROCK,         GROUND,       NOT_VERY_EFFECTIVE
@@ -75,11 +84,10 @@ TypeEffects:
 	db ROCK,         BUG,          SUPER_EFFECTIVE
 	db ROCK,         ICE,          SUPER_EFFECTIVE
 	db GHOST,        NORMAL,       NO_EFFECT
-	db GHOST,        PSYCHIC_TYPE, NO_EFFECT
-	db FIRE,         DRAGON,       NOT_VERY_EFFECTIVE
-	db WATER,        DRAGON,       NOT_VERY_EFFECTIVE
-	db ELECTRIC,     DRAGON,       NOT_VERY_EFFECTIVE
+	db GHOST,        PSYCHIC_TYPE, SUPER_EFFECTIVE ;Harzen 03/04/2023
+	;db ELECTRIC,     DRAGON,       NOT_VERY_EFFECTIVE ;Harzen 03/04/2023 commentata
 	db GRASS,        DRAGON,       NOT_VERY_EFFECTIVE
 	db ICE,          DRAGON,       SUPER_EFFECTIVE
 	db DRAGON,       DRAGON,       SUPER_EFFECTIVE
+  db PSYCHIC_TYPE, BUG,					 NO_EFFECT ;Harzen 03/04/2023
 	db -1 ; end
